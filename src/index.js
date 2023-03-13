32f6bdeba7e6cda40fa6f586253fb0f95e05884b
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-03-13 21:54:52 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Click display sign up form 
+const x = document.querySelector('.signin')
+const model = document.querySelector('.modal')
+x.addEventListener('click', function () {
+  model.classList.add('display')
+})
