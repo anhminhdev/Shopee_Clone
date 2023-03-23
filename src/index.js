b4d58d4d57ec157f423ece3f4c951c9bd57b29bf
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-03-23 20:36:17 +0700

    add event click log in form

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 // Click display sign up form 
 const x = document.querySelector('.signin')
-const model = document.querySelector('.modal')
-x.addEventListener('click', function () {
-  model.classList.add('display')
+const model_signup = document.querySelector('.modal-signup')
+x.addEventListener('click', () => {
+  model_signup.classList.add('display')
+})
+
+// Click display log in form 
+const y = document.querySelector('.signup')
+const model_login = document.querySelector('.modal-login')
+y.addEventListener('click', () => {
+  model_login.classList.add('display')
 })
